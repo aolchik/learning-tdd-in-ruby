f4f9be2dbf15fdf896aa078950f5aed6a7af0dfc
Alejandro Olchik <aolchik@ionatec.com.br>
2016-01-17 19:15:03 -0200

    Terminal notification added.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'rake', '10.5.0'
+gem 'terminal-notifier', '1.6.3'
 gem 'test-unit', '3.1.7'
+gem 'test-unit-notify', '1.0.4'
--- test/simple/test_fibonacci.rb
+++ test/simple/test_fibonacci.rb
@@ -1,8 +1,9 @@
 require './app/simple/fibonacci.rb'
 require 'test/unit'
+require 'test/unit/notify'
 
 class FibonacciTest < Test::Unit::TestCase
-  def test_first
+  def test_pending
     assert(false)
   end
 end
